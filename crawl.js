86c7675afc7aaadfc2e8dd6100993c293d24f0eb
Jacob Ilagan <jilagan5@myseneca.ca>
2026-01-01 19:45:10 -0500

    Url tests

--- crawl.js
+++ crawl.js
@@ -1,7 +1,12 @@
 function normalizeURL(urlString) {
   const urlObject = new URL(urlString);
 
-  return `${urlObject.hostname}${urlObject.pathname}`;
+  const hostPath = `${urlObject.hostname}${urlObject.pathname}`;
+
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
 }
 
 module.exports = {
--- crawl.test.js
+++ crawl.test.js
@@ -10,8 +10,26 @@ test("normalizeURL strip protocol", () => {
   expect(actual).toEqual(expected);
 });
 
-test("normalizeURL strip protocol", () => {
-  const input = "https://blog.boot.dev/path";
+test("normalizeURL strip trailing slash", () => {
+  const input = "https://blog.boot.dev/path/";
+
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL capitals", () => {
+  const input = "https://BLOG.boot.dev/path";
+
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip http", () => {
+  const input = "http://blog.boot.dev/path";
 
   const actual = normalizeURL(input);
   const expected = "blog.boot.dev/path";
